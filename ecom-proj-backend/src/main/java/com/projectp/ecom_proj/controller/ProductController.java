1028699d9ec8af9302566e08c66ebdbe45df7b3f
prajwalnaveeenn <139122004+prajwalnaveeenn@users.noreply.github.com>
2025-03-19 21:42:40 +0530

    Added java files

---
+++ ecom-proj-backend/src/main/java/com/projectp/ecom_proj/controller/ProductController.java
@@ -0,0 +1,99 @@
+package com.projectp.ecom_proj.controller;
+
+import com.projectp.ecom_proj.model.Product;
+import com.projectp.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World";
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProd(@PathVariable int id){
+        Product product = service.getProd(id);
+
+        if(product!=null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+
+        try{
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1,HttpStatus.CREATED);
+        }
+
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProd(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = service.updateProduct(id, product, imageFile);
+            if(product1!=null){
+                return new ResponseEntity<>("Updated", HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+            }
+        }
+        catch (IOException e){
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product1 = service.getProd(id);
+        if(product1!=null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Product Deleted", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Not found", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        System.out.println("Print statement is: " + keyword);
+        List<Product> product = service.searchProducts(keyword);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+}
